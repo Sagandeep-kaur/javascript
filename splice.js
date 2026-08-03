1489cbddac8b7c962c1e180bc31e27f9d95f154f
Sagandeep-kaur <115706027+Sagandeep-kaur@users.noreply.github.com>
2026-08-03 18:38:28 +0530

    Add files via upload

---
+++ splice.js
@@ -0,0 +1,8 @@
+let cars = ['Benz', 'Innova', 'Breeza', 'Etios', 'Dzire'];
+cars.splice(2, 0, 'ambassedor', 'BMW', 'Audi');
+console.log("cars :", cars);
+
+let toys = ['Benz', 'Innova', 'Breeza', 'Etios', 'Dzire'];
+toys.splice(2, 1, 'amb');
+console.log("toys :", toys);
+
