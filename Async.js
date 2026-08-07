64b6ad915d185f26c838b283c8fe07743c2569fb
Sagandeep-kaur <115706027+Sagandeep-kaur@users.noreply.github.com>
2026-08-07 20:53:38 +0530

    Update Async.js

--- Async.js
+++ Async.js
@@ -1,15 +1,14 @@
 async function fetchData() {
-    try {
-      
-      let  response = await fetch('https://api.example.com/data');
-     
-      let dataa = await response.json();
-      console.log(dataa);
-    } catch (error) {
-      console.log('data');
-      console.error('Error fetching data:', error);
-    }
+  try {
+    let response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+
+    let dataa = await response.json();
+
+    console.log(dataa);
+  } catch (error) {
+    console.log('data');
+    console.error('Error fetching data:', error);
   }
+}
 
-
- fetchData() 
+fetchData();
