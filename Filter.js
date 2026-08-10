2b39674c01f1ccd23ea113e8405bed6f173c0de9
Sagandeep-kaur <115706027+Sagandeep-kaur@users.noreply.github.com>
2026-08-10 19:12:28 +0530

    Add files via upload

---
+++ Filter.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3, 4];
+
+const tripledNumbers = numbers.filter(function(number){
+    return number >2})
+
+console.log(tripledNumbers)
+
+const tripledNumberss = numbers.filter(number =>
+    number >1)
+
+    console.log(tripledNumberss)
