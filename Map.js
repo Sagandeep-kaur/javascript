69c1401ebc6db058424812cff2cbcc5d9b09204b
Sagandeep-kaur <115706027+Sagandeep-kaur@users.noreply.github.com>
2026-08-10 09:30:51 +0530

    Add files via upload

---
+++ Map.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4];
+
+const doubledNumbers = numbers.map((number) => {
+    return number * 2;
+});
+
+console.log(doubledNumbers);
+
+let arr= [2, 4, 8, 10]
+let updatedArr = arr.map(val=> val+2)
+console.log(arr);
+console.log(updatedArr);
+
